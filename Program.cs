a2092e193aa4b931895ca8cb8ee022fe5faa70c8
Thiago Cerutti <thiago.cerutti@way2.com.br>
2022-06-09 16:12:41 -0300

    implementado subtração

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine($"Sum sample: {Sum(1, 2)}");
+Console.WriteLine($"Sum sample: {Subtract(1, 2)}");
 
 static int Sum(int a, int b)
 {
@@ -10,7 +11,7 @@ static int Sum(int a, int b)
 
 static int Subtract(int a, int b)
 {
-    throw new NotImplementedException();
+    return a - b;
 }
 
 static int Multiply(int a, int b)
